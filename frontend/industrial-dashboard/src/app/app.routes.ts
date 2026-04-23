83e40332b3a6bdc04eba199d9c6719009c28b947
kukrejankit <ankit.kukreja.89@gmail.com>
2026-04-23 11:57:17 -0500

    Update app.routes.ts

--- frontend/industrial-dashboard/src/app/app.routes.ts
+++ frontend/industrial-dashboard/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ClientDataCollectorComponent } from './pages/client-data-collector/clie
 import { ClientFormComponent } from './pages/client-form/client-form';
 import { LoginComponent } from './pages/login/login';
 import { AssetDetailComponent } from './pages/asset-detail/asset-detail';
+import { FvmDataRequestComponent } from './pages/fvm-data-request/fvm-data-request';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -12,6 +13,7 @@ export const routes: Routes = [
   { path: '',          component: DashboardComponent,           pathMatch: 'full', canActivate: [authGuard] },
   { path: 'interview', component: InterviewToolComponent,                          canActivate: [authGuard] },
   { path: 'data-collector', component: ClientDataCollectorComponent,               canActivate: [authGuard] },
+  { path: 'fvm-data-request', component: FvmDataRequestComponent,                 canActivate: [authGuard] },
   { path: 'assets/:id', component: AssetDetailComponent,                           canActivate: [authGuard] },
   { path: 'form/:id',  component: ClientFormComponent },
   { path: '**', redirectTo: '' }
