e1d191605c74aabc519c3d8176602a93c31de252
kukrejankit <ankit.kukreja.89@gmail.com>
2026-05-16 10:05:55 -0500

    Update environment.prod.ts

--- frontend/industrial-dashboard/src/environments/environment.prod.ts
+++ frontend/industrial-dashboard/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://industrial-ml-api.azurewebsites.net/api',
-  hubUrl: 'https://industrial-ml-api.azurewebsites.net/hubs/sensors'
+  apiUrl: 'https://industrial-ml-api-v2.azurewebsites.net/api',
+  hubUrl: 'https://industrial-ml-api-v2.azurewebsites.net/hubs/sensors'
 };
