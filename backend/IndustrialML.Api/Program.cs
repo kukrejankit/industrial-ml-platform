7d4ef916a6809573eb8352f4c2c73b510f8af26d
kukrejankit <ankit.kukreja.89@gmail.com>
2026-05-17 15:24:07 -0500

    Update Program.cs

--- backend/IndustrialML.Api/Program.cs
+++ backend/IndustrialML.Api/Program.cs
@@ -50,7 +50,7 @@ try
 {
     using var scope = app.Services.CreateScope();
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
 catch (Exception ex)
 {
